096d7134c6bb3d4cb242790f7308ae7650af7b1a
tangjm <jaredtang@hotmail.co.uk>
2021-10-20 17:07:55 +0100

    Test 3 passes

--- src/BankAccount.js
+++ src/BankAccount.js
@@ -1,13 +1,16 @@
 class BankAccount {
 	credit;
 	debit;
+	balance;
 
 	deposit(amount) {
 		this.credit += amount;
+		this.balance += amount;
 	}
 
 	withdraw(amount) {
 		this.debit -= amount;
+		this.balance -= amount;
 	}
 }
 
