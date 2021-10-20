b6af925fc29236ae5b8dc9e0a10f464b691d29de
tangjm <jaredtang@hotmail.co.uk>
2021-10-20 17:28:55 +0100

    Created failing Test 2 of date.spec

--- spec/date.spec.js
+++ spec/date.spec.js
@@ -2,13 +2,14 @@ const Date = require(`../src/Date`);
 
 describe("Test suite for Date class", () => {
 	let expectedOutput, actualOutput;
+	let date;
 
 	beforeEach(() => {
-
+		date = new Date();
 	})
 
 	afterEach(() => {
-
+		date = null;
 	})
 
 	it("Test 1: Testing that we can create a date and specify the day", () => {
@@ -19,6 +20,19 @@ describe("Test suite for Date class", () => {
 
 		// Act
 		actualOutput = date.getDay();
+
+		// Assert
+		expect(actualOutput).toEqual(expectedOutput);
+	})
+
+	it("Test 2: Testing that we can create a date and specify the month", () => {
+		// Arrange
+		const date = new Date();
+		date.setMonth(10);
+		expectedOutput = 10;
+
+		// Act
+		actualOutput = date.getMonth();
 
 		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
