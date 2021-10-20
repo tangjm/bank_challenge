c3702bb1beddb94e344aa4eb790d1aeed2a9bd55
tangjm <jaredtang@hotmail.co.uk>
2021-10-20 16:49:02 +0100

    Created failing Test 2

--- spec/bankAccount.spec.js
+++ spec/bankAccount.spec.js
@@ -14,4 +14,17 @@ describe("Test suite for BankAccount class", () => {
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
+	it("Test 2: TEsting that user can withdraw money", () => {
+		// Arrange
+		let bankAccount = new BankAccount();
+		bankAccount.debit = 10;
+		let expectedOutput = 0;
+
+		// Act
+		bankAccount.withdraw(10);
+		let actualOutput = bankAccount.debit;
+		// Assert
+		expect(actualOutput).toEqual(expectedOutput);
+	})
+
 })
