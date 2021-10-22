ff8f426fb85ded5f48bc625138f710ae6c35afd1
tangjm <jaredtang@hotmail.co.uk>
2021-10-22 16:49:04 +0100

    Test 2 of bankStatement.spec passes

--- src/BankStatement.js
+++ src/BankStatement.js
@@ -1,7 +1,12 @@
+const BankAccount = require("./BankAccount");
+
 class BankStatement {
 	#header;
+	#formattedTransactions;
+
 	constructor() {
 		this.#header = "date || credit || debit || balance";
+		this.#formattedTransactions = [];
 	}
 
 	// Getters
@@ -10,6 +15,60 @@ class BankStatement {
 		return this.#header;
 	}
 
+	getFormattedTransactions() {
+		return this.#formattedTransactions;
+	}
+	// Methods
+
+	format(transactionsArr = new BankAccount().getTransactions()) {
+		for (let i = 0, j = transactionsArr.length; i < j; i++) {
+			let formattedArr = [];
+			if (transactionsArr[i].getType() === "deposit") {
+				// transactionsArr[i] = {
+				// 	dateObj, 
+				// 	type,
+				// 	amount,
+				// 	newBalance
+				// }
+				// let formattedArr = [];
+				// formattedArr[0] = date;
+				// formattedArr[1] = amount to 2.d.p + " ||";
+				// formattedArr[2] = newBalance to 2.d.p;
+
+				formattedArr[0] = this.dateFormat(transactionsArr[i].getDate()); // dateObj
+
+				formattedArr[1] = this.creditFormat(transactionsArr[i].getCredit()); // amount
+
+				formattedArr[2] = this.balanceFormat(transactionsArr[i].getNewBalance()); // newBalance
+
+			} else {
+
+			}
+			this.#formattedTransactions.push(formattedArr.join(" || "));
+		}
+	}
+
+	balanceFormat(balance) {
+		let formattedBalance = balance.toFixed(2); // 2 d.p;
+		return formattedBalance;
+	}
+	creditFormat(amount) {
+		let formattedAmount = amount.toFixed(2) + " ||";
+		return formattedAmount;
+	}
+	debitFormat(amount) {
+		return formattedAmount;
+	}
+	dateFormat(dateObj) {
+		let formattedDate = [dateObj.getDay(), dateObj.getMonth(), dateObj.getYear()].join("/");
+		return formattedDate;
+	}
+
+	depositFormat(transactionObj) {
+	}
+
+	withdrawalFormat() {
+	}
 }
 
 module.exports = BankStatement;
