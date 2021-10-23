abf14b16d5abec979cf189eb93f6d0ad01271ee5
tangjm <jaredtang@hotmail.co.uk>
2021-10-23 14:56:29 +0100

    Refactored statementPrinter.spec

--- spec/statementPrinter.spec.js
+++ spec/statementPrinter.spec.js
@@ -29,21 +29,21 @@ describe("Test suite for StatementPrinter", () => {
 		testBankStatement = null;
 	})
 
-	it("Test 1: Testing that we can print out the headers for the bank statement", () => {
+	it("Test 1: Prints out the headers for the bank statement", () => {
 		spyOn(console, "log");
 		statementPrinter.printHeader(testBankStatement);
 
 		expect(console.log).toHaveBeenCalledWith("header");
 	})
 
-	it("Test 2: Testing that we can print out the entire recorded transaction history", () => {
+	it("Test 2: Prints out the entire recorded transaction history", () => {
 		spyOn(console, "log");
 		statementPrinter.printTransactions(testBankStatement);
 
 		expect(console.log).toHaveBeenCalledTimes(2)
 	})
 
-	it("Test 3: Testing that the recorded transaction history is printed out in reverse chronological order", () => {
+	it("Test 3: Transaction history is printed out in reverse chronological order", () => {
 		spyOn(console, "log");
 		statementPrinter.printTransactions(testBankStatement);
 
@@ -51,18 +51,18 @@ describe("Test suite for StatementPrinter", () => {
 		expect(console.log.calls.argsFor(1)).toEqual([1]);
 	})
 
-	it("Test 4: Testing that the print() method prints both the header and transactions", () => {
-		spyOn(statementPrinter, "printTransactions")
-		spyOn(statementPrinter, "printHeader")
+	it("Test 4: 'print()' method prints both header and transactions", () => {
+		spyOn(statementPrinter, "printTransactions");
+		spyOn(statementPrinter, "printHeader");
 		statementPrinter.print(testBankStatement);
 
 		expect(statementPrinter.printHeader).toHaveBeenCalled();
 		expect(statementPrinter.printTransactions).toHaveBeenCalled();
 	})
 
-	it("Test 5: Testing that the print() method prints the header first", () => {
-		let spy1 = spyOn(statementPrinter, "printTransactions")
-		let spy2 = spyOn(statementPrinter, "printHeader")
+	it("Test 5: 'print()' method prints the header first", () => {
+		let spy1 = spyOn(statementPrinter, "printTransactions");
+		let spy2 = spyOn(statementPrinter, "printHeader");
 		statementPrinter.print(testBankStatement);
 
 		expect(spy2).toHaveBeenCalledBefore(spy1);
