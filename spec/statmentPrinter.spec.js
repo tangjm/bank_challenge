bef9802b1f92472a2e9b0f7eb46e39cb9692cf49
tangjm <jaredtang@hotmail.co.uk>
2021-10-23 12:04:40 +0100

    Refactored test 1 of statementPrinter.spec

--- spec/statmentPrinter.spec.js
+++ spec/statmentPrinter.spec.js
@@ -1,25 +1,30 @@
-const BankAccount = require("../src/BankAccount");
-const BankStatement = require("../src/BankStatement");
-const Date = require("../src/Date");
 const StatementPrinter = require("../src/StatementPrinter");
+
+
+class TestBankStatement {
+	#header;
+	constructor() {
+		this.#header = "header";
+	}
+	getHeader() {
+		return this.#header;
+	}
+}
 
 describe("Test suite for StatementPrinter", () => {
 	it("Test 1: Testing that we can print out the headers for the bank statement", () => {
 		// Arrange
 		let statementPrinter = new StatementPrinter();
-		let bankAccount = new BankAccount();
-		let date = new Date(10, 01, 2012);
-		bankAccount.deposit(1000, date);
-		let bankStatement = new BankStatement()
-		bankStatement.format(bankAccount.getTransactions());
+		let testBankStatement = new TestBankStatement();
 
 		// Act
-		actualOutput = statementPrinter.printHeader(bankStatement);
-		spyOn(bankStatement, "getHeader").and.returnValue("header");
+		let spy = spyOn(console, "log");
+		statementPrinter.printHeader(testBankStatement);
 
 		// Assert
-		expect(console.log).toHaveBeenCalledWith("header");
+		expect(spy).toHaveBeenCalledWith("header");
 	})
+
 	// it("Test 1: Testing that the StatementPrinter is able to print out the entire recorded transaction history", () => {
 	// 	// Arrange
 
