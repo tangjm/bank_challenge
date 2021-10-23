f3bbdccfa8e3eda01a0aeccc1406e7f3e89ec72c
tangjm <jaredtang@hotmail.co.uk>
2021-10-23 15:47:33 +0100

    Decoupled bankStatment.spec

--- spec/bankStatement.spec.js
+++ spec/bankStatement.spec.js
@@ -1,52 +1,61 @@
-const BankAccount = require("../src/BankAccount");
 const BankStatement = require("../src/BankStatement");
-const Date = require(`../src/Date`);
 
 describe("Test suite for BankStatement class", () => {
-	it("Test 1: Testing that BankStatement formats the statement header", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let bankStatement = new BankStatement();
-		let dateObj = new Date(10, 10, 2021);
-		bankAccount.deposit(10, dateObj);
+	let bankStatement;
+	let dateObj, transactionObj;
 
+	beforeEach(() => {
+		bankStatement = new BankStatement();
+		dateObj = {
+			day: 10,
+			month: 10,
+			year: 2021,
+			getDay() { return this.day },
+			getMonth() { return this.month },
+			getYear() { return this.year }
+		}
+	})
+
+	afterEach(() => {
+		bankStatement = null;
+		dateObj = null;
+	})
+
+	it("Test 1: BankStatement correctly formats statement header", () => {
 		expectedOutput = "date || credit || debit || balance";
-		// Act
 		actualOutput = bankStatement.getHeader();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
-	it("Test 2: Testing that each recorded deposit transaction is properly formatted", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let bankStatement = new BankStatement();
-		let dateObj = new Date(10, 10, 2021);
-		bankAccount.deposit(10, dateObj);
+	it("Test 2a: Each recorded deposit transaction is correctly formatted", () => {
+		transactionObj = {
+			getType() { return "deposit" },
+			getDate() { return dateObj },
+			getCredit() { return 10 },
+			getDebit() { return 10 },
+			getNewBalance() { return 10 }
+		}
 		expectedOutput = "10/10/2021 || 10.00 || || 10.00";
+		bankStatement.format([transactionObj]);
 
-		// Act
-		bankStatement.format(bankAccount.getTransactions());
 		actualOutput = bankStatement.getFormattedTransactions()[0];
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
-	it("Test 2b: Testing that each recorded withdrawal transaction is properly formatted", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let bankStatement = new BankStatement();
-		let dateObj = new Date(10, 10, 2021);
-		bankAccount.withdraw(10, dateObj);
+	it("Test 2b: Each recorded withdrawal transaction is properly formatted", () => {
+		bankStatement.format([{
+			getType() { return "" },
+			getDate() { return dateObj },
+			getCredit() { return 10 },
+			getDebit() { return 10 },
+			getNewBalance() { return -10 }
+		}]);
 		expectedOutput = "10/10/2021 || || 10.00 || -10.00";
 
-		// Act
-		bankStatement.format(bankAccount.getTransactions());
 		actualOutput = bankStatement.getFormattedTransactions()[0];
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 })
