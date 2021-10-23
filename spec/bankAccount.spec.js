27906f3fc4694885236ca362b1f0b88927f3fcf1
tangjm <jaredtang@hotmail.co.uk>
2021-10-23 16:20:17 +0100

    Decoupled bankAccount.spec

--- spec/bankAccount.spec.js
+++ spec/bankAccount.spec.js
@@ -1,7 +1,4 @@
 const BankAccount = require(`../src/BankAccount`);
-const Transaction = require(`../src/Transaction`);
-const Date = require(`../src/Date`);
-
 
 describe("Test suite for despositing and withdrawing money", () => {
 	let expectedOutput, actualOutput;
@@ -46,6 +43,18 @@ describe("Test suite for despositing and withdrawing money", () => {
 
 describe("Test suite for transaction storage", () => {
 	let expectedOutput, actualOutput;
+	let bankAccount, dateObj;
+
+	beforeEach(() => {
+		bankAccount = new BankAccount();
+		dateObj = {};
+	})
+
+	afterEach(() => {
+		bankAccount = null;
+		dateObj = null;
+	})
+
 	it("Test 4: All transactions are recorded", () => {
 		bankAccount.withdraw(20);
 		expectedOutput = 1;
@@ -56,87 +65,56 @@ describe("Test suite for transaction storage", () => {
 	})
 
 	it("Test 5a: Deposit transactions are recorded with a date", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
+		bankAccount.deposit(10, dateObj);
 		expectedOutput = dateObj;
 
-		// Act
-		bankAccount.deposit(10, dateObj);
 		actualOutput = bankAccount.getTransactions()[0].getDate();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
 	it("Test 5b: Withdrawal transactions are recorded with a date", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
+		bankAccount.withdraw(10, dateObj);
 		expectedOutput = dateObj;
 
-		// Act
-		bankAccount.withdraw(10, dateObj);
 		actualOutput = bankAccount.getTransactions()[0].getDate();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
 	it("Test 6a: Deposit transactions are recorded with the correct type", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
 		bankAccount.deposit(10, dateObj);
 		expectedOutput = "deposit";
 
-		// Act
-		// We spyOn the transactionObj's constructor
 		actualOutput = bankAccount.getTransactions()[0].getType();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
 	it("Test 6b: Withdrawal transactions are recorded with the correct type", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
 		bankAccount.withdraw(10, dateObj);
 		expectedOutput = "withdrawal";
 
-		// Act
 		actualOutput = bankAccount.getTransactions()[0].getType();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
 	it("Test 7a: Deposit transactions are recorded with correct deposit amount", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
 		bankAccount.deposit(10, dateObj);
 		expectedOutput = 10;
 
-		// Act
 		actualOutput = bankAccount.getTransactions()[0].getCredit();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 
 	it("Test 7b: Withdrawal transactions are recorded with correct withdrawal amount", () => {
-		// Arrange
-		let bankAccount = new BankAccount();
-		let dateObj = new Date(10, 1, 2012);
 		bankAccount.withdraw(10, dateObj);
 		expectedOutput = 10;
 
-		// Act
 		actualOutput = bankAccount.getTransactions()[0].getDebit();
 
-		// Assert
 		expect(actualOutput).toEqual(expectedOutput);
 	})
 })
